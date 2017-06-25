cfc0e2ed3eb88581de9a7f5c48996e3b7accda7f
Ben McClure <ben.mcclure@gmail.com>
2017-06-24 22:59:56 -0500

    Revamp some of the basic functions
    This does break semver, but this is pre-alpha

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var async = require("async"),
-    iplink = require("./iplink"),
-    iwconfig = require("./iwconfig"),
-    ifconfig = require("./ifconfig"),
-    iwlist = require("./iwlist"),
-    netctl = require('./netctl');
+var async = require("async")
+var iplink = require("./iplink")
+var iwconfig = require("./iwconfig")
+var ifconfig = require("./ifconfig")
+var iwlist = require("./iwlist")
+var netctl = require('./netctl')
 
 /*****************************************************************************\
     Return a set of functions which we can use to manage and check our wifi
@@ -15,71 +15,108 @@ module.exports = {
     restart_interface: _restart_interface,
     scan_networks: _scan_networks,
     is_wifi_enabled: _is_wifi_enabled,
-    enable_wifi: _enable_wifi
+    is_wifi_connected: _is_wifi_connected,
+    save_profile: _save_profile,
+    delete_profile: _delete_profile,
+    enable_wifi: _enable_wifi,
+    disable_wifi: _disable_wifi
 }
 
 function _get_wifi_interface(callback) {
-    iwconfig.get_interface(callback);
+    iwconfig.get_interface(callback)
 }
 
 // Get generic info on an interface
 function _get_wifi_info(wifi_interface, callback) {
-    var output = [];
+    var output = []
 
     async.series([
         function run_ifconfig(next_step) {
             ifconfig.status(wifi_interface, function (error, info) {
                 if (!error) output.concat(info);
                 next_step(error);
-            });
+            })
         },
         function run_iwconfig(next_step) {
             iwconfig.status(wifi_interface, function (error, info) {
                 if (!error) output.concat(info);
                 next_step(error);
-            });
+            })
         },
     ], function(error) {
-        return callback(error, output);
-    });
+        return callback(error, output)
+    })
 }
 
 function _restart_interface(wifi_interface, callback) {
-    iplink.restart(wifi_interface, callback);
+    iplink.restart(wifi_interface, callback)
 }
 
 function _scan_networks(wifi_interface, callback) {
-    iwlist(wifi_interface, callback);
+    iwlist(wifi_interface, callback)
 }
 
-function _is_wifi_enabled(wifi_interface, callback) {
+function is_wifi_enabled(wifi_interface, callback) {
+    netctl.is_auto_enabled(wifi_interface, callback)
+}
+
+function _is_wifi_connected(wifi_interface, callback) {
     get_wifi_info(wifi_interface, function(error, info) {
-        if (error) return callback(error, null);
+        if (error) return callback(error, null)
 
         var return_info = null;
         if ("<unknown>" != info["inet_addr"] &&
             "<unknown>" == info["unassociated"]) {
-            return_info = info["inet_addr"];
+            return_info = info["inet_addr"]
         }
 
-        return callback(null, return_info);
-    });
+        return callback(null, return_info)
+    })
 }
 
-// Disables AP mode and reverts to wifi connection
-function _enable_wifi(wifi_interface, connection_info, callback) {
+function _save_profile(wifi_interface, connection_info, callback) {
     async.series([
         function save_wifi_profile(next_step) {
-            netctl.save_wifi_profile(wifi_interface, connection_info, next_step);
+            netctl.save_wifi_profile(wifi_interface, connection_info, next_step)
         },
         function restart_netctl_auto(next_step) {
-            netctl.restart_auto(wifi_interface, next_step);
+            netctl.restart_auto(wifi_interface, next_step)
+        }
+    ], callback)
+}
+
+function _delete_profile(wifi_interface, ssid, callback) {
+    async.series([
+        function delete_wifi_profile(next_step) {
+            netctl.delete_wifi_profile(wifi_interface, connection_info.wifi_ssid, next_step)
+        },
+        function restart_netctl_auto(next_step) {
+            netctl.restart_auto(wifi_interface, next_step)
+        }
+    ], callback)
+}
+
+function _enable_wifi(wifi_interface, callback) {
+    async.series([
+        function restart_netctl_auto(next_step) {
+            netctl.restart_auto(wifi_interface, next_step)
         },
         function restart_network_interface(next_step) {
-            restart_network_interface(wifi_interface, next_step);
+            restart_network_interface(wifi_interface, next_step)
         },
         function enable_netctl_auto(next_step) {
-            netctl.enable_auto(wifi_interface, next_step);
+            netctl.enable_auto(wifi_interface, next_step)
         }
-    ], callback);
+    ], callback)
+}
+
+function _disable_wifi(wifi_interface, callback) {
+    async.series([
+        function stop_netctl_auto(next_step) {
+            netctl.stop_auto(wifi_interface, next_step)
+        },
+        function disable_netctl_auto(next_step) {
+            netctl.disable_auto(wifi_interface, next_step)
+        }
+    ], callback)
 }
--- netctl.js
+++ netctl.js
@@ -1,26 +1,26 @@
-var _ = require("underscore")._,
-    async = require("async"),
-    fs = require("fs"),
-    systemctl = require("systemctl"),
-    exec = require("child_process").exec;
+var _ = require("underscore")._
+var async = require("async")
+var fs = require("fs")
+var systemctl = require("systemctl")
+var exec = require("child_process").exec
 
 // Better template format
 _.templateSettings = {
     interpolate: /\{\{(.+?)\}\}/g,
     evaluate :   /\{\[([\s\S]+?)\]\}/g
-};
+}
 
 // Helper function to write a given template to a file based on a given context
 function write_template_to_file(template_path, file_name, context, callback) {
     async.waterfall([
         function read_template_file(next_step) {
-            fs.readFile(template_path, {encoding: "utf8"}, next_step);
+            fs.readFile(template_path, {encoding: "utf8"}, next_step)
         },
         function update_file(file_txt, next_step) {
-            var template = _.template(file_txt);
-            fs.writeFile(file_name, template(context), next_step);
+            var template = _.template(file_txt)
+            fs.writeFile(file_name, template(context), next_step)
         }
-    ], callback);
+    ], callback)
 }
 
 module.exports = {
@@ -29,37 +29,47 @@ module.exports = {
     start_auto: _start_auto,
     stop_auto: _stop_auto,
     restart_auto: _restart_auto,
-    save_wifi_profile: _save_wifi_profile
+    is_auto_enabled: _is_auto_enabled,
+    save_wifi_profile: _save_wifi_profile,
+    delete_wifi_profile: _delete_wifi_profile
 }
 
 function _enable_auto(interface_name, callback) {
-    systemctl.enable("netctl-auto@" + interface_name, callback);
+    systemctl.enable("netctl-auto@" + interface_name, callback)
 }
 
 function _disable_auto(interface_name, callback) {
-    systemctl.disable("netctl-auto@" + interface_name, callback);
+    systemctl.disable("netctl-auto@" + interface_name, callback)
 }
 
 function _stop_auto(interface_name, callback) {
-    systemctl.stop("netctl-auto@" + interface_name, callback);
+    systemctl.stop("netctl-auto@" + interface_name, callback)
 }
 
 function _start_auto(interface_name, callback) {
-    systemctl.start("netctl-auto@" + interface_name, callback);
+    systemctl.start("netctl-auto@" + interface_name, callback)
+}
+
+function _is_auto_enabled(interface_name, callback) {
+    systemctl.is_enabled('netctl-auto@' + interface_name, callback)
 }
 
 function _restart_auto(interface_name, callback) {
-    systemctl.restart("netctl-auto@" + interface_name, callback);
+    systemctl.restart("netctl-auto@" + interface_name, callback)
 }
 
 function _save_wifi_profile(wifi_interface, connection_info, callback) {
     async.waterfall([
         function read_template_file(next_step) {
-            fs.readFile("./templates/netctl/wifi.template", {encoding: "utf8"}, next_step);
+            fs.readFile("./templates/netctl/wifi.template", {encoding: "utf8"}, next_step)
         },
         function update_file(file_txt, next_step) {
-            var template = _.template(file_txt);
-            fs.writeFile("/etc/netctl/" + wifi_interface, template(connection_info), next_step);
+            var template = _.template(file_txt)
+            fs.writeFile("/etc/netctl/" + wifi_interface + '-' + connection_info.wifi_ssid, template(connection_info), next_step)
         }
-    ], callback);
+    ], callback)
+}
+
+function _delete_wifi_profile(wifi_interface, wifi_ssid, callback) {
+    fs.unlink('/etc/netctl/' + wifi_interface + '-' + wifi_ssid, callback)
 }
